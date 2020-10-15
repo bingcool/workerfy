20ab4821ba17d7b63135e1fdf886bdbde104a3b2
bingcool <2437667702@qq.com>
2020-10-15 19:07:14 +0800

    add redis subscribe test

---
+++ tests/Subscribe/Master.php
@@ -0,0 +1,55 @@
+#!/usr/bin/php
+<?php
+ini_set('memory_limit','20M');
+define("START_SCRIPT_ROOT", __DIR__);
+define("START_SCRIPT_FILE", __FILE__);
+date_default_timezone_set('Asia/Shanghai');
+
+// 默认在当前目录runtime下
+define("PID_FILE_ROOT", '/tmp/workerfy/log/Subscribe');
+
+$pid_file = PID_FILE_ROOT.'/'.pathinfo(__FILE__)['filename'].'.pid';
+define("PID_FILE", $pid_file);
+
+$dir_config = dirname(__DIR__);
+$root_path = dirname($dir_config);
+
+include $root_path."/vendor/autoload.php";
+
+$config_file_path = $dir_config."/Config/config.php";
+
+$Config = \Workerfy\ConfigLoad::getInstance();
+$Config->loadConfig($config_file_path);
+
+$processManager = \Workerfy\processManager::getInstance();
+
+$process_name = 'test-subscribe';
+$process_class = \Workerfy\Tests\Subscribe\Worker::class;
+
+// 订阅的话只需要一个进程订阅，多个的话会重复订阅，重复处理逻辑，造成业务错误
+$process_worker_num = 1;
+
+$async = true;
+$args = [
+    'wait_time' => 1
+];
+$extend_data = null;
+$processManager->createCliPipe(true);
+$processManager->addProcess($process_name, $process_class, $process_worker_num, $async, $args, $extend_data);
+
+$processManager->onStart = function ($pid) {
+
+    file_put_contents(PID_FILE, $pid);
+
+};
+
+$processManager->onCreateDynamicProcess = function ($process_name, $process_num) use($processManager) {
+    $this->createDynamicProcess($process_name, $process_num);
+};
+
+
+$processManager->onExit = function() use($config_file_path) {
+    //var_dump("master exit",$config_file_path);
+};
+
+$master_pid = $processManager->start();
